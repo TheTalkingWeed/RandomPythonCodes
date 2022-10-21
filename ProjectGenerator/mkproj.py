905904b5cf8613d6fb3ccb41c6c425bda7f7d0bd
Alex_sz1 <szolnoki.alex699@gmail.com>
2022-10-21 19:18:07 +0200

    Update mkproj.py

--- ProjectGenerator/mkproj.py
+++ ProjectGenerator/mkproj.py
@@ -1,9 +1,17 @@
 import os
 import sys
+import shutil
+
 
 def check_dir(dir_name):
     sub_dirs =[name for name in os.listdir("./") if os.path.isdir(os.path.join("./", name))]
     return dir_name in sub_dirs
+
+def check_file(file_name):
+    files = os.listdir("./")
+    sub_files = [name for name in files if os.path.isfile("./"+'/'+name)]
+
+    return file_name in sub_files
 
 def check_lanuage(lang):
     lang_list = ["java","py","c","cpp","cs","js","html"]
@@ -87,51 +95,112 @@ def get_hello_world(filename,lang):
         )
 
 
-def create_source_code(lang,path):
-    class_name = input("Please type the name for your main file: ");
+def create_source_code(lang,path,class_name):
     file_name =class_name +"." + lang
-    write_file(path +"/" + file_name,get_hello_world(class_name,lang))
+    
+    if check_file(file_name):
+        need_frewrite = input("File already exists do you want to rewrite it? (y/n)")
+        if need_frewrite.lower() == "y":
+            write_file(path +"/" + file_name,get_hello_world(class_name,lang))
+
+        else:
+            print("Yout source code wasn't created")
+
+    else:
+        write_file(path +"/" + file_name,get_hello_world(class_name,lang))
+
     if lang == "html":
         need_css = input("Do you want a css file?(y/n)")
 
         if need_css.lower() == "y":
             file_name = class_name + ".css"
             write_file(path +"/" + file_name,get_hello_world(class_name,"css"))
-
+        else:
+            print("There will be no css file")
+    print("Source code has been created.")
 
 def create_proj(name,lang):
 
     if check_dir(name):
-        print("Project already exists ")
+        need_rewrite = input("Project already exists do you want to rewrite it? (y/n)")
+        if need_rewrite.lower() == "y":
+            shutil.rmtree(name)
+            parent_dir = "./"
+            path = os.path.join(parent_dir, name)
+            class_name = input("Please type the name for your main file: ");
+
+            os.mkdir(path)
+            create_source_code(lang, path,class_name)
+            print(name,"project has been made")
+            sys.exit()
+        else:
+            print("Yout project wasn't created")
         sys.exit()
     parent_dir = "./"
     path = os.path.join(parent_dir, name)
+    class_name = input("Please type the name for your main file: ");
+
     os.mkdir(path)
-    create_source_code(lang, path)
+    create_source_code(lang, path,class_name)
     print(name,"project has been made")
 
 def handle_arguments(arg):
+
+
+
     if len(arg)-1 == 0:
         print("No command line arguments try -help")
         sys.exit()
 
     if arg[1] == "-help":
-        print("mkproj [your project name] [programming language]")
-        print("Languages: java,py,c,cpp,cs,js,html(write them like this)")
+        print("-sl\t - List the supported languages")
+        print("-cr\t - Create a new project (mkproj -cr [your project name] [programming language])")
+        print("-crf\t - Create a new file (mkproj -crf [your file name] [programming language])")
+        sys.exit()
+
+    if arg[1] == "-sl":
+        lang_list = ["java","py","c","cpp","cs","js","html"]
+
+        print("Java\t\t java")
+        print("Python\t\t py")
+        print("C\t\t c")
+        print("C++\t\t cpp")
+        print("C#\t\t cs")
+        print("JavaScript\t js")
+        print("HTML\t\t html")
+        sys.exit()
+
+    if arg[1] == "-cr":
+        if len(arg) == 4:
+            if check_lanuage(arg[3]):
+                create_proj(arg[2],arg[3])
+            else: print("There is no language named: ",arg[3])
+        else:
+            print("Missing command line arguments use -help")
+        sys.exit()
+
+
+    if arg[1] == "-crf":
+        if len(arg) == 4:
+            if check_lanuage(arg[3]):
+                create_source_code(arg[3],"./",arg[2])
+            else: print("There is no language named: ",arg[3])
+        else:
+            print("Missing command line arguments use -help")
+        sys.exit()
+
+
+
+    if arg[1][0] == "-":
+        print(arg[1],"is an unknown command use -help")
         sys.exit()
 
     if len(arg)-1 == 1 :
-        print("Please select a language")
-        sys.exit()
-    if arg[1][0] == "-":
-        print("Unknown command try -help")
+        print("Please select a language,to see the supported languages use \"mkproj -sl\" ")
         sys.exit()
 
-    if len(arg) == 3:
-        if check_lanuage(arg[2]):
-            create_proj(arg[1],arg[2])
-        else: print("There is no language named: ",arg[2])
-        sys.exit()
+
+
     if len(arg) > 3:
         print("To mutch arguments try -help")
         sys.exit()
