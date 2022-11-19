c6d41b170f1fa8191adf4b53cd90928236efedb4
Alex_sz1 <szolnoki.alex699@gmail.com>
2022-11-19 14:45:32 +0100

    javítva

--- kisFeladatok/mp3download.py
+++ kisFeladatok/mp3download.py
@@ -9,7 +9,7 @@ def check_file(file_name,des):
     return file_name in sub_files
 
 def load_links(file_name):
-    if check_file(file_name):
+    if check_file(file_name,"./"):
         with open(file_name,'r') as f:
             contents = f.read().splitlines()
         return contents
@@ -18,29 +18,31 @@ def load_links(file_name):
 
 
 
-def download(link):
-    print("egy")
-    yt = YouTube(link)
-    video = yt.streams.filter(only_audio=True).first()
-    print("ketto")
-    destination = "C:\\Users\\Alex\\Music"
-    print("itt van ")
+def download(linkek):
+    letoltve = 0
+    for link in linkek:
+        yt = YouTube(link)
+        video = yt.streams.filter(only_audio=True).first()
+        destination = "C:\\Users\\Alex\Music\\ad"
 
-    if check_file(yt.title + ".mp3",destination):
-        print("A zene mar letoltve: ",yt.title)
-    else:
+        if check_file(yt.title + ".mp3",destination):
+            print("A zene mar letoltve: ",yt.title)
+        else:
 
-        out_file = video.download(output_path=destination)
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
-        print(yt.title + " has been successfully downloaded.")
+            out_file = video.download(output_path=destination)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+            print(yt.title + " has been successfully downloaded.")
+        letoltve += 1
+
+        print(letoltve,"/",len(linkek))
 
 
 def main():
-    #links = load_links("linkek.txt")
+    links = load_links("linkek.txt")
 
-    download("https://www.youtube.com/watch?v=27rPtLv60fc")
+    download(links)
 
 if __name__ == "__main__":
     main()
